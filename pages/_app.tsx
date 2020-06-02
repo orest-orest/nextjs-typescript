e696f44750892ae21fc5e94d4fb310729841dcaa
orest <52675004+orest-forest@users.noreply.github.com>
2020-06-02 19:18:36 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { Provider } from 'react-redux'
 import { useStore } from '../store'
 
-//@ts-ignore
+
 export default function App ({ Component, pageProps }) {
   const store = useStore(pageProps.initialReduxState)
 
